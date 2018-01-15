c5907f77763d4f864a3e61f420649bfae8c71dba
Louise Johnson <louisemaejohnson@gmail.com>
2018-01-15 22:09:57 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,24 @@
+function getFirstSelector(selector) {
+  return document.querySelector(selector);
+}
+
+function nestedTarget() {
+  var nested = document.getElementById("nested");
+  return nested.getElementsByClassName("target")[0];
+}
+
+function increaseRankBy(n) {
+ var rank = document.getElementById("app");
+ const lis = rank.querySelectorAll('ul.ranked-list li');
+ for (let i = 0; i < lis.length; i++) {
+   lis[i].innerHTML = i+1+n;
+ }
+}
+
+function deepestChild() {
+  var grandNode = document.querySelectorAll("grand-node");
+  while (grandNode.hasChild()) {
+    grandNode = grandNode.querySelector('div');
+  }
+  return 
+}
