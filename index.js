a894d2d19a9669cee89ac02b9ad1f91a28612354
Louise Johnson <louisemaejohnson@gmail.com>
2018-01-15 22:58:27 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,12 +1,22 @@
+
+/* Accepts a selector and returns the first element 
+ * that matches.
+ */
 function getFirstSelector(selector) {
   return document.querySelector(selector);
 }
 
+/* Pulls an element with class .target out of 
+ * an element with id #nested.
+ */
 function nestedTarget() {
   var nested = document.getElementById("nested");
   return nested.getElementsByClassName("target")[0];
 }
 
+/* Increases the ranks in all of the elements with
+ * class .ranked-list by n. 
+ */
 function increaseRankBy(n) {
  var rank = document.getElementById("app");
  const lis = rank.querySelectorAll('ul.ranked-list li');
@@ -15,6 +25,9 @@ function increaseRankBy(n) {
  }
 }
 
+/* Pulls out the most deeply nested child from 
+ * div#grand-node. 
+ */
 function deepestChild() {
   var grandNode = document.getElementById("grand-node");
   while (grandNode.querySelector('div') !== null) {
